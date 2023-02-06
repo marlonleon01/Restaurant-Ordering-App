42052732a5d77d266ed62b4e56c463b843717a7e
Marlon Leon <ghostveene@gmail.com>
2023-02-06 09:57:05 -0500

    added link to imgs of food

--- data.js
+++ data.js
@@ -4,20 +4,20 @@ export const menuArray = [
         ingredients: ["pepperoni", "mushrom", "mozarella"],
         id: 0,
         price: 14,
-        emoji: "🍕"
+        images: "images/Pizza Img.png"
     },
     {
         name: "Hamburger",
         ingredients: ["beef", "cheese", "lettuce"],
         price: 12,
-        emoji: "🍔",
+        images: "images/Burger Img.png",
         id: 1
     },
         {
         name: "Beer",
         ingredients: ["grain, hops, yeast, water"],
         price: 12,
-        emoji: "🍺",
+        images: "images/Beer Img.png",
         id: 2
     }
 ]
