176c00c86439a084677ec40b07b8483d1fd11fa7
Marlon Leon <ghostveene@gmail.com>
2023-02-06 10:11:14 -0500

    changed imgs string to only name of img

--- data.js
+++ data.js
@@ -4,20 +4,20 @@ export const menuArray = [
         ingredients: "pepperoni, mushrom, mozarella",
         id: 0,
         price: 14,
-        images: "images/Pizza Img.png"
+        images: "Pizza Img.png"
     },
     {
         name: "Hamburger",
         ingredients: "beef, cheese, lettuce",
         price: 12,
-        images: "images/Burger Img.png",
+        images: "Burger Img.png",
         id: 1
     },
         {
         name: "Beer",
         ingredients: "grain, hops, yeast, water",
         price: 12,
-        images: "images/Beer Img.png",
+        images: "Beer Img.png",
         id: 2
     }
 ]
