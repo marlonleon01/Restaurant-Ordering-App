af862c6275515a5366dea6c0ba3ea3947117dce9
Marlon Leon <ghostveene@gmail.com>
2023-02-06 10:10:23 -0500

    changed ingredients to string instead of array of strings

--- data.js
+++ data.js
@@ -1,21 +1,21 @@
 export const menuArray = [
     {
         name: "Pizza",
-        ingredients: ["pepperoni", "mushrom", "mozarella"],
+        ingredients: "pepperoni, mushrom, mozarella",
         id: 0,
         price: 14,
         images: "images/Pizza Img.png"
     },
     {
         name: "Hamburger",
-        ingredients: ["beef", "cheese", "lettuce"],
+        ingredients: "beef, cheese, lettuce",
         price: 12,
         images: "images/Burger Img.png",
         id: 1
     },
         {
         name: "Beer",
-        ingredients: ["grain, hops, yeast, water"],
+        ingredients: "grain, hops, yeast, water",
         price: 12,
         images: "images/Beer Img.png",
         id: 2
