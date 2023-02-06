339b8e461f4e4a4c3e5a10fd4aa7c1935bf853ac
Marlon Leon <ghostveene@gmail.com>
2023-02-06 13:27:15 -0500

    add btn working need to use remove btn

--- index.js
+++ index.js
@@ -1,45 +1,92 @@
 import { menuArray } from "./data.js";
 
 const allContent = document.getElementById("all-content")
+const menuItemsContainer = document.getElementById("menu-items-container")
+const orderContainer = document.getElementById("order-container")
+
+
+let totalPrice = 0
+let order = []
+
+document.addEventListener("click", event => {
+    if (event.target.dataset.add) {
+        addOrderItem(event.target.dataset.add)
+    }
+})
+
+const addOrderItem = (itemId) => {
+    const targetItemObj = menuArray.filter(item => {
+        return item.id == itemId
+    })[0]
+
+    if (!order.includes(targetItemObj)) {
+        order.push(targetItemObj)
+        totalPrice += targetItemObj.price
+        renderOrderSection()
+        renderOrderItems()
+    }
+}
 
 const getMenuHtml = () => {
     let menuHtml = ""
     
     menuArray.forEach(items => {
-        menuHtml += `<div class="menu-items">
-                                    <img src="images/${items.images}" class="menu-img">
-                                    <div class="item-contents">
-                                        <h2 class="menu-item-title">${items.name}</h2>
-                                        <p class="item-description">${items.ingredients}</p>
-                                        <h3 class="item-price">$${items.price}</h3>
-                                    </div>
-                                        <div class="icon">
-                                            <i class="fa-regular fa-plus"></i>
-                                        </div>            
-                                </div>`   
+        menuHtml += `
+                    <div class="menu-items">
+                        <img src="images/${items.images}" class="menu-img">
+                        <div class="item-contents">
+                            <h2 class="menu-item-title">${items.name}</h2>
+                            <p class="item-description">${items.ingredients}</p>
+                            <h3 class="item-price">$${items.price}</h3>
+                        </div>
+                            <div class="icon">
+                                <i class="fa-regular fa-plus" data-add="${items.id}"></i>
+                            </div>            
+                    </div>
+                    `   
     })
     return menuHtml
 }
 
-const renderMenu = () => allContent.innerHTML += getMenuHtml()
+const renderMenu = () => menuItemsContainer.innerHTML = getMenuHtml()
 
 renderMenu()
 
-const renderOrder = () => {
-    allContent.innerHTML += `
-                            <div class="your-order">
-                            <h1 class="order-title">Your Order</h1>
-                            <div class="order-items">
-                                <h3 class="order-item">Pizza 
-                                    <span class="remove-item" id="remove-item">remove</span>
-                                </h3>
-                                <h4 class="order-item-price">$14</h4>
-                            </div>
-                            <div class="order-final-price">
-                                <h3 class="price-title">Total Price:</h3>
-                                <h4 class="price-number">$26</h4>
-                            </div>
-                            <button name="complete order button">Complete order</button>
-                            </div>
-                            `
+const getOrderSectionHtml = () => {
+    return `
+            <div class="your-order">
+            <h1 class="order-title">Your Order</h1>
+            <div class="order-items" id="order-items">
+                <!-- Insert Order Items Here -->
+            </div>
+            <div class="order-final-price">
+                <h3 class="price-title">Total Price:</h3>
+                <h4 class="price-number">$${totalPrice}</h4>
+            </div>
+            <button name="complete order button">Complete order</button>
+            </div>
+            `
+}
+
+const renderOrderSection = () => {
+    orderContainer.innerHTML = getOrderSectionHtml()
+}
+
+const getOrderItemHtml = () => {
+    let orderHtml = "" 
+    order.forEach(item => {
+        orderHtml += `
+                        <div class="order-individual-item">
+                            <h3 class="order-item">${item.name} 
+                                <span class="remove-item" data-remove="${item.id}">remove</span>
+                            </h3>
+                            <h4 class="order-item-price">$${item.price}</h4>
+                        </div>
+                        `
+    })
+    return orderHtml
+}
+
+const renderOrderItems = () => {
+    document.getElementById("order-items").innerHTML = getOrderItemHtml()
 }
