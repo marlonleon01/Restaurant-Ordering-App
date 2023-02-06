bfdedfd6e40a37f4074e98463c4bef4acb797030
Marlon Leon <ghostveene@gmail.com>
2023-02-06 12:18:44 -0500

    seperated menutml and render html

--- index.js
+++ index.js
@@ -2,23 +2,28 @@ import { menuArray } from "./data.js";
 
 const allContent = document.getElementById("all-content")
 
-const renderAllContent = () => {
-    for (let menu of menuArray) {
-        allContent.innerHTML += `<div class="menu-items">
-                                    <img src="images/${menu.images}" class="menu-img">
+const getMenuHtml = () => {
+    let menuHtml = ""
+    
+    menuArray.forEach(items => {
+        menuHtml += `<div class="menu-items">
+                                    <img src="images/${items.images}" class="menu-img">
                                     <div class="item-contents">
-                                        <h2 class="menu-item-title">${menu.name}</h2>
-                                        <p class="item-description">${menu.ingredients}</p>
-                                        <h3 class="item-price">$${menu.price}</h3>
+                                        <h2 class="menu-item-title">${items.name}</h2>
+                                        <p class="item-description">${items.ingredients}</p>
+                                        <h3 class="item-price">$${items.price}</h3>
                                     </div>
                                         <div class="icon">
                                             <i class="fa-regular fa-plus"></i>
                                         </div>            
                                 </div>`   
-    }
+    })
+    return menuHtml
 }
 
-renderAllContent()
+const renderMenu = () => allContent.innerHTML += getMenuHtml()
+
+renderMenu()
 
 const renderOrder = () => {
     allContent.innerHTML += `
@@ -38,5 +43,3 @@ const renderOrder = () => {
                             </div>
                             `
 }
-
-renderOrder()
